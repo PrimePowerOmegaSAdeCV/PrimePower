a52c858d7fe4fa09b2163db4af3f6aa0f266463b
SurielOdoo <slo@odoo.com>
2018-07-08 00:11:36 -0700

    [REF] added an identation in an if

--- primepower_sales_extended/models/sale.py
+++ primepower_sales_extended/models/sale.py
@@ -1,11 +1,11 @@
 from odoo import api, fields, models, tools, SUPERUSER_ID, _
 
 class SaleOrderLine(models.Model):
-    
+
     _inherit = 'sale.order.line'
-    
+
     product_values_ids = fields.One2many('returned.values','sale_line_id', string="Valores del producto", copy=False)
-    
+
     @api.onchange('product_id')
     def onchange_product_id(self):
         
@@ -14,9 +14,9 @@ class SaleOrderLine(models.Model):
             list_vals = []
             record['product_values_ids'] = [(6,0,list_vals)]
             if record.product_id.categ_id.sale_template_id:
-            for template_value_id in record.product_id.categ_id.sale_template_id.values_ids:
-              values = {'name':template_value_id.name, 'field_type' : template_value_id.field_type}
-              list_vals.append((6,0,values))
-            self.product_values_ids = [list_vals]
+                for template_value_id in record.product_id.categ_id.sale_template_id.values_ids:
+                  values = {'name':template_value_id.name, 'field_type' : template_value_id.field_type}
+                  list_vals.append((6,0,values))
+                self.product_values_ids = [list_vals]
         return vals
-    
+
