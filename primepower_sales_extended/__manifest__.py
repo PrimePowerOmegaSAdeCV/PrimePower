2a3cbc385d05f3eec2d49d252f9ce0de5063f121
SurielOdoo <slo@odoo.com>
2018-07-07 22:19:28 -0700

    [ADD] added new module with scaffold option

---
+++ primepower_sales_extended/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sale extend for Prime Power",
+
+    'summary': """
+        Adds extra fields in sales orders""",
+
+    'description': """
+        Sale product detail
+=================
+Add extra fields needed when capturing a sale.
+    """,
+
+    'author': "Suriel",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Sale',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['sale'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ primepower_sales_extended/models/__init__.py
@@ -0,0 +1,4 @@
+from . import sales_template
+from . import sale
+from . import product_category
+
