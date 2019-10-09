5e2c5e99b287f7a4336000a623219de93f80fa23
ehe <ehe@odoo.com>
2019-10-09 21:28:48 +0000

    [MIG] Change of report arrangement

--- pp_sale/models/sale_order.py
+++ pp_sale/models/sale_order.py
@@ -37,7 +37,9 @@ class SaleOrder(models.Model):
         self.ensure_one()
         report_pages = [[]]
 
-        optional_product_images = self.sale_order_option_ids.mapped('product_id.product_image_ids')
+        # optional_product_images = self.sale_order_option_ids.mapped('product_id.product_image_ids')
+        # [MIG] change to the main image
+        optional_product_images = self.sale_order_option_ids.mapped('product_id.image')
 
         for category, lines in groupby(self.sale_order_option_ids, lambda l: l.layout_category_id):
             # If last added category induced a pagebreak, this one will be on a new page
@@ -53,47 +55,6 @@ class SaleOrder(models.Model):
             })
 
         return report_pages, optional_product_images
-
-    # @api.multi
-    # def optional_product_lines_layouted(self):
-    #     """
-    #     Returns this order lines classified by sale_layout_category and separated in
-    #     pages according to the category pagebreaks. Used to render the report.
-    #     """
-    #     self.ensure_one()
-    #     report_pages = [[]]
-    #     # print('Now entering the optional layout...')
-    #     # print(self.optional_product_line)
-    #
-    #     optional_product_ids = self.order_line.mapped('product_id.optional_product_ids').mapped('product_variant_ids')
-    #
-    #     # filtering out those product that are already in the regular order line
-    #     regular_product_ids = self.order_line.mapped('product_id')
-    #     optional_product_ids -= regular_product_ids
-    #
-    #     # see if there are images to display
-    #     # optional_product_images_exist = any(optional_product_ids.filtered(lambda p: p.product_image_ids))
-    #     optional_product_images = optional_product_ids.mapped('product_image_ids')
-    #     # print(optional_product_ids)
-    #
-    #     # create those lines for report purpose
-    #     optional_lines = [self.env['sale.order.line'].create({'order_id': self.id, 'product_id': optional_product_id.id, 'optional': True}) for optional_product_id in optional_product_ids]
-    #     # print(optional_lines)
-    #
-    #     for category, lines in groupby(optional_lines, lambda l: l.layout_category_id):
-    #         # If last added category induced a pagebreak, this one will be on a new page
-    #         if report_pages[-1] and report_pages[-1][-1]['pagebreak']:
-    #             report_pages.append([])
-    #         # Append category to current report page
-    #
-    #         report_pages[-1].append({
-    #             'name': category and category.name or _('Uncategorized'),
-    #             'subtotal': category and category.subtotal,
-    #             'pagebreak': category and category.pagebreak,
-    #             'optional_product_lines': list(lines),
-    #         })
-    #
-    #     return report_pages, optional_product_images
 
 
 class SaleOrderLine(models.Model):
@@ -118,6 +79,7 @@ class SaleOrderOption(models.Model):
     _inherit = 'sale.order.option'
 
     delivery_time_id = fields.Many2one('sale.order.line.delivery.time', ondelete='cascade', string='Delivery Time')
+    layout_category_id = fields.Many2one('sale.layout_section', ondelete="set null", string='Section')
 
 
 
