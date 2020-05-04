467c23ae41f415ff20d4bd91685dc28d86868c48
AxeldelosReyes <shelonga@live.com>
2020-05-04 16:04:55 -0500

    L10n_mx_edi default values
    -added edi fields default values for each contact.

---
+++ default_edi_values/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Edi MX Default Values',
+    'version': '13',
+    'category': '',
+    'description': """ get data from partner  and fill in the Invoice.
+    """,
+    'author':'Xmarts',
+    'depends': ['base','account'],
+    'data': [
+	'views/partner_view.xml',
+    ],
+    'qweb': [
+        ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
