6264ab9b14e18a226333c087bbc16a68dec33421
Manuel De Los Reyes <manuel9410z@gmail.com>
2020-05-09 10:08:04 -0500

    pp rm

--- pp_sale/models/res_users.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
-
-class ResUsers(models.Model):
-    _inherit='res.users'
-
-    signature_custom = fields.Html('Custom Signature (for SO report)')
-
