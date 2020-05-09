a6c7afaf6d8b5cf4bfed8dc05db0deb8a65a88f7
Manuel De Los Reyes <manuel9410z@gmail.com>
2020-05-09 10:26:22 -0500

    only others

--- pp_sale/models/res_partner_bank.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
-
-
-class ResPartnerBank(models.Model):
-    _inherit = 'res.partner.bank'
-
-    transfer_code = fields.Char('Transfer Code')
--- primepower_sales_extended/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import stock
-from . import sales_template
-from . import sale
-from . import product_category
-from . import product
-from . import res_partner
-
-
