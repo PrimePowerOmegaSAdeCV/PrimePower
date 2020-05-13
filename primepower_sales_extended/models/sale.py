58b5e1aff23fa0c9546f6ea7a330e6cde4af7de8
AxeldelosReyes <shelonga@live.com>
2020-05-12 22:16:02 -0500

    Sale and Invoice Qweb Report and optional products.
    fix optional products on create.
    fix translation on sale report
    fix alignment on work order
    fix invoice header and colors.

--- primepower_sales_extended/models/sale.py
+++ primepower_sales_extended/models/sale.py
@@ -10,6 +10,33 @@ class SaleOrder(models.Model):
     shipping_payer_id = fields.Many2one('stock.shipping.payer', required=False, copy=True, string="Pago de flete", states={'draft': [('readonly', False)], 'sent': [('readonly', False)], 'done' : [('readonly',True)], 'sale' : [('readonly',True)]})
     shipping_notes = fields.Text(string="Notas de salida de almacen", required=False, copy=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)], 'done' : [('readonly',True)], 'sale' : [('readonly',True)]})
 
+    def create(self, values):
+        vals = super(SaleOrder, self).create(values)
+        for line in self.order_line.filtered(lambda x: not x.display_type):
+            if line.product_id in self.sale_order_option_ids.mapped(lambda x: x.product_id):
+                continue
+            options = self.env['sale.order.option']
+            for product_template in self.env['product.product'].browse(line.product_id.id).optional_product_ids:
+                product = product_template.product_variant_id
+                product = product.with_context(lang=self.partner_id.lang)
+                pricelist = self.pricelist_id
+                partner_id = self.partner_id.id
+                price_unit = pricelist.with_context(uom=product.uom_id.id).get_product_price(product, 1, partner_id)
+                name = product.name
+                if product.description_sale:
+                    name += '\n' + product.description_sale
+                values = {
+                    'product_id': product.id,
+                    'order_id': self.id,
+                    'price_unit': price_unit,
+                    'website_description': product.website_description,
+                    'name': name,
+                    'uom_id': product.uom_id.id,
+                }
+                option_id = options.create(values)
+        return vals
+
+
 class SaleOrderLine(models.Model):
 
     _inherit = 'sale.order.line'
@@ -50,34 +77,34 @@ class SaleOrderLine(models.Model):
         
     
         
-    def create(self, values):
-        vals = super(SaleOrderLine, self).create(values)
-        if 'order_id' in values:
-            options = self.env['sale.order.option']
-            for product_template in self.env['product.product'].browse(values['product_id']).optional_product_ids:
-                product = product_template.product_variant_id
-                sale_options = options.search([('product_id','=',product.id)])
-                if not sale_options: 
-                    product = product.with_context(lang=self.order_id.partner_id.lang)                   
-                    order = self.env['sale.order'].browse(values['order_id'])
-                    pricelist = order.pricelist_id
-                    partner_id = order.partner_id.id
-                    price_unit = pricelist.with_context(uom=product.uom_id.id).get_product_price(product, 1, partner_id)
-                    name = product.name
-                    if product.description_sale:
-                        name += '\n' + product.description_sale
-            
-                    values = {
-                        'product_id' : product.id,
-                        'order_id' : order.id,
-                        'price_unit' : price_unit,
-                        'website_description' : product.website_description,
-                        'name' : name,
-                        'uom_id' : product.uom_id.id,
-                    }
-                    option_id = options.create(values)        
-        
-        return vals
+    # def create(self, values):
+    #     vals = super(SaleOrderLine, self).create(values)
+    #     if 'order_id' in values:
+    #         options = self.env['sale.order.option']
+    #         for product_template in self.env['product.product'].browse(values['product_id']).optional_product_ids:
+    #             product = product_template.product_variant_id
+    #             order = self.env['sale.order'].browse(values['order_id'])
+    #
+    #             product = product.with_context(lang=self.order_id.partner_id.lang)
+    #
+    #             pricelist = order.pricelist_id
+    #             partner_id = order.partner_id.id
+    #             price_unit = pricelist.with_context(uom=product.uom_id.id).get_product_price(product, 1, partner_id)
+    #             name = product.name
+    #             if product.description_sale:
+    #                 name += '\n' + product.description_sale
+    #
+    #             values = {
+    #                 'product_id' : product.id,
+    #                 'order_id' : order.id,
+    #                 'price_unit' : price_unit,
+    #                 'website_description' : product.website_description,
+    #                 'name' : name,
+    #                 'uom_id' : product.uom_id.id,
+    #             }
+    #             option_id = options.create(values)
+    #
+    #     return vals
             
         
         
