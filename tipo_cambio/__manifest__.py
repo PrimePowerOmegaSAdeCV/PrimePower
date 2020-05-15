364e39889b43524777c2e5c67a78667c05bb08a6
AxeldelosReyes <shelonga@live.com>
2020-05-15 15:07:33 -0500

    Tipo de cambio change
    Fix dependency

--- tipo_cambio/__manifest__.py
+++ tipo_cambio/__manifest__.py
@@ -1,16 +1,16 @@
 {
     'name': 'Xmarts Tipo de Cambio ',
-    'version': '11.0.3',
+    'version': '13.0.3',
     'category': "",
     'description': """ get data from currency_id 
     """,
     'author':'Xmarts',
-    'depends': ['account','sale','account_accountant', 'informe_cotizacion'],
+    'depends': ['account','sale','account_accountant'],
     'data': [
 	'views/sale_order_view.xml',
 	'views/invoice_view.xml',
 	'views/purchase_order_view .xml',
-	'report/reporte_sale.xml',
+	# 'report/reporte_sale.xml',
 	  
     ],
     'qweb': [
