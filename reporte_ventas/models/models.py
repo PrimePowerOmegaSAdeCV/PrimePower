1111e53ca04af54376bbf1f94dd1ee9246f77266
Manuel De Los Reyes <manuel9410z@gmail.com>
2020-05-17 22:29:39 -0500

    rm un fields

--- reporte_ventas/models/models.py
+++ reporte_ventas/models/models.py
@@ -14,9 +14,7 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     move_origin = fields.Char(related="move_id.invoice_origin", store=True)
-    delivery_apr = fields.Selection([('1', "Aprobado"), ('2', "No Aprobado")], related="move_id.delivery_apr",
-                                    store=True)
-
+    
 
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
@@ -36,8 +34,6 @@ class reporte_ventas(models.Model):
     fecha_de_pedido = fields.Datetime(string="Fecha de pedido")
     orden_de_compra = fields.Char(string="Orden de compra")
     fecha_prevista = fields.Char('Fecha Prevista')
-    tiempo_serv = fields.Char(string="Tiempo Servicio")
-    tiempo_ven = fields.Char(string="Tiempo Entrega V.")
     ###############################################################
     product_id = fields.Many2one('product.product', string="Producto")
     precio_unitario = fields.Float()
@@ -45,7 +41,6 @@ class reporte_ventas(models.Model):
     ###############################################################
     invoice_name = fields.Char(string="Numero de Factura")
     fecha_factura = fields.Date(string="Fecha de factura")
-    envio_aprobado = fields.Selection([('1', "Aprobado"), ('2', "No Aprobado")], string="Envió aprobado")
     ###############################################################
     movimiento = fields.Char(string="Movimiento")
     guia = fields.Char(string="Guía")
@@ -81,24 +76,17 @@ CREATE OR REPLACE VIEW {} AS (
    sl.product_id as product_id,
    sl.price_unit as precio_unitario,
    sl.product_uom_qty as cantidad,
-   CASE sl.tiempo_ven WHEN '1' THEN 'Inmediato' WHEN '2' THEN '3 Dias Habiles' END AS tiempo_ven,
-   CASE sl.tiempo_serv WHEN '1' THEN 'Inmediato' WHEN '2' THEN '3 Dias Habiles' END AS tiempo_serv,
-   sl.scheduled_date2 as fecha_prevista,
-   
    --  Invoice(per line)
    aml.move_name as invoice_name,
    aml.date as fecha_factura,
-   aml.delivery_apr as envio_aprobado,
    --  Stock picking por medio de stock moves lines type_id y tabla (moves) de arriba.
    sml.reference as movimiento,
         (SELECT guia FROM moves WHERE moves.picking_id = sml.picking_id) as guia,
         (SELECT mensajeria FROM moves WHERE moves.picking_id = sml.picking_id) as mensajeria,
         (SELECT fecha_envio FROM moves WHERE moves.picking_id = sml.picking_id) as fecha_envio,
         (SELECT recibido FROM moves WHERE moves.picking_id = sml.picking_id) as recibido
-
  FROM sale_order so
-   Right Join sale_order_line sl on so.id = sl.order_id and so.type_id NOT in (Select id FROM sale_order_type sop WHERE sop.name  ILIKE 'Saldo%')
-    Left join account_move_line aml on aml.id in (SELECT invoice_line_id FROM sale_order_line_invoice_rel WHERE order_line_id = sl.id )
+   Left join account_move_line aml on aml.id in (SELECT invoice_line_id FROM sale_order_line_invoice_rel WHERE order_line_id = sl.id )
    Left join stock_move_line sml on so.name = sml.stock_move_origin and sl.id = sml.move_sale_line_id
       and sml.picking_id IN (Select picking_id FROM moves)
     )""".format(self._table))
