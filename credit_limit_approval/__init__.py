891f594f461363d7b33f5489ccbdc605ec4b712a
Manuel De Los Reyes <manuel9410z@gmail.com>
2020-05-17 21:44:31 -0500

    credit fix

--- credit_limit_approval/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding:utf-8 -*-
-from . import models
-<<<<<<< Updated upstream
-from . import wizards
-
-=======
-
-from . import wizards
->>>>>>> Stashed changes
--- credit_limit_approval/wizards/credit_wizard.py
+++
@@ -1,5 +0,0 @@
-# -*- encoding:utf-8 -*-
-from odoo import models, fields, api, tools, _
-
-class SaleConfirmLimit(models.TransientModel):
-    _name = 'sale.control.limit.wizard'
