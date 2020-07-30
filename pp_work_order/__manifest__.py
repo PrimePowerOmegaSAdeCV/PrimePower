e26595571851b69de28a7b7b4352ba4f6e66af6b
BeopenMx <shelonga@live.com>
2020-07-30 01:47:12 -0500

    Separate Work Order from pp_sale_extended

---
+++ pp_work_order/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Prime Power Work Order',
+    'version': '13',
+    'category': "",
+    'description': """ Work Order Report 
+    """,
+    'author':'Xmarts',
+    'depends': ['base','sale','primepower_sales_extended'],
+    'data': [
+	"report/work_order_report.xml",
+    ],
+    'qweb': [
+        ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
