ad9850d27f794142efe6b60e467cfb61c2324225
BeopenMx <shelonga@live.com>
2020-08-20 13:49:59 -0500

    Default Edi Values
    -Fix Sale Order to Invoice Values.

--- default_edi_values/models/invoice.py
+++ default_edi_values/models/invoice.py
@@ -6,7 +6,7 @@ class AccountMove(models.Model):
 
     def _get_partner_edi_values(self):
         if self.partner_id:
-            self.l10n_mx_edi_payment_method_id = self.partner_id.l10n_mx_edi_payment_method_id
+            self.l10n_mx_edi_payment_method_id = self.partner_id.l10n_mx_edi_payment_method_id.id
             self.l10n_mx_edi_usage = self.partner_id.l10n_mx_edi_usage
         return
 
@@ -14,3 +14,15 @@ class AccountMove(models.Model):
     def _onchange_partner_id(self):
         self._get_partner_edi_values()
         return super(AccountMove, self)._onchange_partner_id()
+
+    @api.model
+    def create(self, vals):
+        edi_fields = ['l10n_mx_edi_payment_method_id', 'l10n_mx_edi_usage']
+        if any(fields not in vals for fields in edi_fields):
+            move = self.new(vals)
+            move._get_partner_edi_values()
+            for field in edi_fields:
+                if field not in vals and move[field]:
+                    vals[field] = move._fields[
+                        field].convert_to_write(move[field], move)
+        return super(AccountMove, self).create(vals)
