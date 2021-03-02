58e29a3971fb55def2b5e8a1067e2a5a72d882ca
Axel-BeOpenMx <shelonga@live.com>
2021-03-01 21:56:00 -0600

    [ADD] informe_pp_wh

---
+++ informe_pp_wh/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Prime Power Picking Informe',
+    'version': '13.0.0.1',
+    'category': "",
+    'description': """ Reporte warehouse.
+    """,
+    'author':'Munin',
+    'depends': ['base','stock'],
+    'data': [
+        "report/pp_picking.xml",
+        "views/stock_picking_view.xml",
+        "views/res_company_view.xml",
+    ],
+    'qweb': [
+        ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ informe_pp_wh/models/stock_picking.py
@@ -0,0 +1,9 @@
+from odoo import api, fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    motivo_salida = fields.Char(string="Motido de Salida", required=False, copy=False)
+    fecha_regreso = fields.Date(string="Fecha de regreso", required=False, copy=False)
+    motivo_salida_no_regresa = fields.Char(string="Motivo de Salida NO regresa", required=False, copy=False)
