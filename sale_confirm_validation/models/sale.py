1464705070f106586d6ff7d15463136f978dee29
Axel-BeOpenMx <shelonga@live.com>
2021-05-04 14:51:40 -0500

    [FIX] name

--- sale_confirm_validation/models/sale.py
+++ sale_confirm_validation/models/sale.py
@@ -9,7 +9,7 @@ class SaleOrder(models.Model):
         lines = self.order_line.filtered(lambda x: x.product_id.categ_id)
         category_lines = lines.mapped('product_id.categ_id')
         required_category = self.env['product.category'].sudo().with_context(lang="es_MX").search(
-            [('display_name', '=', 'Placa Plana')], limit=1).id
+            [('name', '=', 'Placa Plana')], limit=1).id
         if required_category not in category_lines.ids:
             raise UserError("No se tiene la categoría  requerida: Conectores")
         return super(SaleOrder, self).action_confirm()
